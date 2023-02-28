530cb8d39da974bf8eee9ee74bc7e2b2d2cc867e
NHPI12 <huynguyenxyznick2@gmail.com>
2023-02-28 23:46:56 +0700

    Update labwork 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,37 +1,66 @@
-students = {}
-courses = {}
-marks = {}
-def studentsInput():
-    students_numb = int(input("Enter number of students in the class: "))
-    for i in range(students_numb):
-        id = input("Enter student ID")
-        name = input("Enter student name")
-        DOB = input("Enter date of birth")
-        students[id] = {"name": name, "dob" :DOB}
-def coursesInput():
-    numb_courses = int(input("Enter number of courses: "))
-    for i in range(numb_courses):
-        course_id = input("Enter course  ID")
-        course_name = input("Enter course name")
-        courses[course_id] = {"name": course_name}
-def marksInput():
-    course_id = input("Enter course ID")
-    course_name = input("Enter course name")
-    courses[course_id] = {"name": course_name}
-def coursesList():
-    for course_id in courses:
-        print(f"{course_id}: {courses[course_id]('name')}")
+#data lists
+students = []
+courses = []
+marks = []
+mark_course = []
 
-def studentsList():
-    course_id = input("Enter the course ID: ")
-    if course_id not in courses:
-        print("Invalid course ID")
-        return
-    for id in students:
-        if id in marks and course_id in marks[id]:
-            print(f"{students[id]['name']}: {marks[id][course_id]}")
-        else:
-            print(f"{students[id]['name']}: ")
+#function input number of students
+def input_students_number():
+    numb_students = int(input("Input number of students "))
+    return numb_students
 
+#function input student's info
+def input_students_info(numb_students):
+    for i in range(numb_students):
+        student_id = int(input("Enter student's ID "))
+        student_name = input("Enter student's name ")
+        student_dob = input("Enter student's Date of birth ")
+        student_info = {"ID ":student_id, "Name ":student_name, "Date of birth: ":student_dob}
+        students.append(student_info)
 
+#function input number of courses
+def input_courses_number():
+    numb_courses = int(input("Input number of courses "))
+    return numb_courses
 
+#function input course's info
+def input_course_info(numb_courses):
+    for i in range (numb_courses):
+        course_id = int(input("Input course's ID "))
+        course_name = input("Inpur course's name ")
+        course_info = {"ID: ": course_id, "Name: " : course_name}
+        courses.append(course_info)
+
+#function input marks
+def input_marks_courses(numb_students):
+    course_name = input("Enter courses name: ")
+    for i in range(numb_students):
+        mark = int(input(f'Enter student {students[i]["Name "]} Mark'))
+        mark_info = {"ID ": students[i]["ID "], "Name: ": students[i]["Name "],"Course ": course_name, "Mark": mark}
+        marks.append(mark_info)
+        
+#function list students
+def students_list():
+    for i in range(len(students)):
+        print(students[i])
+#function list courses
+def courses_list():
+    for i in range(len(courses)):
+        print(courses[i])
+
+#function show courses mark
+def show_courses_mark():
+    for i in range(len(marks)):
+        print(marks[i])
+# main program
+
+# student's info input
+numb_students = input_students_number()
+input_students_info(numb_students)
+
+# course's info input
+numb_courses = input_courses_number()
+input_course_info(numb_courses)
+
+#input courses marks
+input_marks_courses(numb_students)
